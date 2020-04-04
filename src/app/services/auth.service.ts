0087dc1e16bd1e2ccb104d26f21136421f8705f0
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-04-04 13:11:56 -0400

    Clase 172. Firebase y servicios REST.

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+  private apikey = 'AIzaSyCfbACN5ZhGhwesYnr1bskbymCXcFfndZ4';
+
+  // crear nuevo usuario
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+  // Login
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  logout() {}
+
+  login( usuario: UsuarioModel ) {}
+
+  nuevoUsuario( usuario: UsuarioModel ) {}
+}
