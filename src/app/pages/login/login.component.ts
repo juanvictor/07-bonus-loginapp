e60d9d785d68e31c970b6f6843ef39f19dec9d6f
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-04-04 21:10:49 -0400

    Clase 176. SweetAlert2 - Mostrar notificaciones al usuario. Se añadio ruteo al home.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +17,8 @@ export class LoginComponent implements OnInit {
   usuario: UsuarioModel;
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private router: RouterModule
   ) { }
 
   ngOnInit() {
@@ -25,11 +30,28 @@ export class LoginComponent implements OnInit {
       return;
     }
 
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
     this.auth.login( this.usuario )
     .subscribe( resp => {
       console.log(resp);
+
+      Swal.close();
+
+      this.router.navigateByUrl('/home');
     }, (err) => {
       console.log(err.error.error.message);
+
+      Swal.fire({
+        icon: 'error',
+        title: 'Error al autentificar',
+        text: err.error.error.message
+      });
     });
 
     // console.log('Imprimir SI el formulario es válido');
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { UsuarioModel } from '../../models/usuario.model';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
@@ -13,7 +17,8 @@ export class RegistroComponent implements OnInit {
   usuario: UsuarioModel;
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -25,11 +30,28 @@ export class RegistroComponent implements OnInit {
       return;
     }
 
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
     this.auth.nuevoUsuario( this.usuario )
       .subscribe( resp => {
         console.log( resp );
+
+        Swal.close();
+
+        this.router.navigateByUrl('/home');
       }, (err) => {
         console.log( err.error.error.message );
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al autentificar',
+          text: err.error.error.message
+        });
       }
     );
 
